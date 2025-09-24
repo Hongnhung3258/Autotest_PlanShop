304fc3cb5726bfcaca4c36ea1532716a8a1e6c85
Hồng Nhung <20010877@st.phenikaa-uni.edu.vn>
2025-09-25 00:58:11 +0700

    Update cypress.config.js

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,7 @@ module.exports = {
 
   e2e: {
     // Dynamic baseUrl - ưu tiên environment variables
-    baseUrl: process.env.CYPRESS_baseUrl || process.env.BASE_URL || 'http://planshop.local/',
+    baseUrl: process.env.CYPRESS_baseUrl || process.env.BASE_URL || 'https://planshop-staging.com/',
     specPattern: 'cypress/e2e/**/*.cy.js',
     screenshotsFolder: 'cypress/screenshots',
     videosFolder: 'cypress/videos',
